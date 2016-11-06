47551708b86a07f6d3a05723fe80e7d3655a2b73
Boullon Daniel <daniel.boullon@yahoo.com>
2016-11-06 18:10:12 -0300

    refactor

--- cliente/src/main/java/mapagrafico/Tile.java
+++ cliente/src/main/java/mapagrafico/Tile.java
@@ -1,102 +1,71 @@
 package mapagrafico;
-
 import java.awt.Graphics2D;
-
-
 
 public class Tile {
 
 	public final static int ANCHO = 64;
 	public final static int ALTO = 32;
-	private int altoImagen;
-	private int x;
-	private int y;
-	private int px; // Pasar de coordenadas logicas a coordenadas reales
-	private int py;	//
-	private int sprite;
-	private boolean obstaculo;
-
-	public int getPx() {
-		return px;
-	}
-	public int getPy() {
-		return py;
-	}
+		
+	protected int xLogica;		// posicion logica de la matriz.
+	protected int yLogica;
+	protected int xIsometrica; 	// posicion real que se va dibujar
+	protected int yIsometrica;	
+	protected int sprite;
+	protected int altoImagen;
 
 
-	public Tile(int x, int y, int sprite, boolean obstaculo) {
 
-		this.x = x;
-		this.y = y;
-		this.sprite = sprite;
-		this.obstaculo = obstaculo;
-		altoImagen = ALTO;
-	}
 	public Tile(int x, int y, int sprite) {
-		this.x = x;
-		this.y = y;
+		this.xLogica = x;
+		this.yLogica = y;
 		this.sprite = sprite;
-		altoImagen = ALTO;
+		this.altoImagen = Tile.ALTO;
 	}
-
 	public Tile(int x, int y, int sprite,int altoImagen) {
-		this.x = x;
-		this.y = y;
+		this.xLogica = x;
+		this.yLogica = y;
 		this.sprite = sprite;
 		this.altoImagen = altoImagen;
-	}
-	
-
-
-	public boolean getObstaculo() {
-		return this.obstaculo;
 	}
 
 	public void dibujar(Graphics2D g2d, int deltaX, int deltaY) {
 		// 0,0 -> 0,0
 		// 1,0 -> 0,16
-		deltaX+=x;
-		deltaY+=y;		
-		px = (deltaX - deltaY) * ( ANCHO / 2);
-		py = (deltaX + deltaY) * ( ALTO / 2);
+		deltaX+=xLogica;
+		deltaY+=yLogica;		
+		xIsometrica = (deltaX - deltaY) * ( ANCHO / 2);
+		yIsometrica = (deltaX + deltaY) * ( ALTO / 2);
 		g2d.drawImage( MapaGrafico.getImage(sprite), 0, 0 , null);			
-
 	}
-
-
 
 	public void mover(Graphics2D g2d, int x2, int y2) {
 
-
-		x2+=x;
-		y2+=y;	
+		x2+=xLogica;
+		y2+=yLogica;	
 
 		int nx = (x2 - y2) * ( ANCHO / 2);
 		int ny = (x2 + y2) * ( ALTO / 2);
-
-		if(py == ny){
-	//		y = y2;
+		
+		if(xIsometrica < nx){
+			xIsometrica+=2;
 		}
-		if(px == nx){
-	//		x = x2;
+		if(xIsometrica > nx){
+			xIsometrica-=2;
 		}
 		
-		if(px < nx){
-			px+=2;
+		if(yIsometrica < ny){
+			yIsometrica++;
 		}
-		if(px > nx){
-			px-=2;
+		if(yIsometrica > ny){
+			yIsometrica--;
 		}
-		
-		if(py < ny){
-			py++;
-		}
-		if(py > ny){
-			py--;
-		}
-		
-
-		g2d.drawImage( MapaGrafico.getImage(sprite), px, py , null);	
+		g2d.drawImage( MapaGrafico.getImage(sprite), xIsometrica, yIsometrica , null);	
 	}
-
+	
+	public int getPx() {
+		return xIsometrica;
+	}
+	public int getPy() {
+		return yIsometrica;
+	}
 }
