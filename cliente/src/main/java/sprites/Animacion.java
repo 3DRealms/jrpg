6ab54781296cfaf876b2e9d04a82a251a9ede680
locodeltacho <alex.serrano94@hotmail.com>
2016-11-07 20:26:14 -0300

    Animacion - primer approach
    Clase animación, lo que hace es de un array de sprites obtener, según un
    índice que se actualiza cada tantos segundos, un frame de ese array. A
    mayor cantidad de frames en el array, más fluidez en el movimiento.
    
    La dejo por ahóra ahí, porque lo de dani está rancio, y en lo de Braian
    estaría casi listo para usar (faltaría cortar los sprites con la class
    Sprite)

---
+++ cliente/src/main/java/sprites/Animacion.java
@@ -0,0 +1,37 @@
+package sprites;
+
+import java.awt.image.BufferedImage;
+
+public class Animacion {
+		private int velocidad;
+		private int i;
+		private long lastTime;
+		private long timer;
+		
+		private BufferedImage[] frames;
+		
+		public Animacion(int velocidad, BufferedImage[] frames) {
+			this.velocidad = velocidad;
+			this.frames = frames;
+			i = 0;
+			//tiempo en milisegundos desde q arranco
+			lastTime = System.currentTimeMillis();
+		}
+			
+		public void actualizar(){
+			timer+= System.currentTimeMillis() - lastTime;
+			lastTime = System.currentTimeMillis() ;
+			
+			if(timer >velocidad){
+				i++;
+				timer = 0;
+				
+				if(i >= frames.length)
+					i = 0;
+			}
+		}
+			
+		public BufferedImage getFrameActual(){
+			return frames[i];
+		}
+}
