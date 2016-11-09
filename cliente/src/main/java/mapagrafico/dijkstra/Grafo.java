0d373a2dd6af3e8b491c1fc5c906dfa687e9c273
locodeltacho <alex.serrano94@hotmail.com>
2016-11-09 12:59:04 -0300

    funcion METODO encontrar vecinos de una celda
    Extraido de las profundidades del Kernighan y Ritchie

--- cliente/src/main/java/mapagrafico/dijkstra/Grafo.java
+++ cliente/src/main/java/mapagrafico/dijkstra/Grafo.java
@@ -77,7 +77,32 @@ public class Grafo {
 
 
 	}
+	
+	public void obtenerVecinos(int fil, int col, int mat[][], int filas, int columnas){
+		int i,j;
+		
+	    for( i =-1; i<2; i++) // recorro matriz de vecinos
+	    {
+	        for(j =-1; j<2; j++)
+	        {
+	            if(esPosicionValida(fil+i,col+j,filas,columnas) ) // descarto a los fuera de rango
+	            {
+	                if( ( fil+i!=fil || col+j!=col) ) // descarto la fila y colugna dados
+	                {
+	                    //Hacer lo que sea que haga con la i,j
+	                }
+	            }
+	        }
+	    }
+	}
+	
+	public boolean esPosicionValida(int pos_f,int pos_c,int filas, int columnas){
+		return (pos_f>=0 && pos_f<filas && pos_c>=0 && pos_c<columnas);
+	}
+	
 }
+
+
 
 
 /*
